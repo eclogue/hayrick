6949ac7779c0741725b6c77d19b3c5e8c8ad986f
bugbear <yuhe.wei@upai.com>
2017-11-17 21:26:25 +0800

    fix paste misstake

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -1,113 +1,185 @@
 <?php
-/**
- * @license MIT
- * @copyright Copyright (c) 2017
- * @author: bugbear
- * @date: 2017/9/10
- * @time: 下午1:20
- */
 
-namespace Bulrush;
+namespace Hayrick\Http;
 
-use Generator;
-use SplStack;
+use Psr\Http\Message\ResponseInterface;
 use RuntimeException;
+use InvalidArgumentException;
+use Hayrick\Environment\Reply;
 
-class Poroutine
+class Response extends Message implements ResponseInterface
 {
 
-    private $coroutine;
+    protected $response;
 
-    private $exception = null;
+    /*
+     * @var array
+     * */
+    protected $headers;
 
-    private $value = null;
+    /*
+     * @var integer
+     * */
+    protected $statusCode = 200;
 
-    private $return = false;
+
+    /*
+     * store the \Swoole\Http\Response instance
+     * */
+    protected $res;
+
+    /*
+     * send body
+     * */
+    protected $body;
+
+    protected $reasonPhrase;
+
+    protected $content;
 
 
-    public function __construct(Generator $co, bool $return)
+
+    public function __construct()
     {
-        $this->coroutine = $this->stack($co);
-        $this->return = $return;
+        $this->headers = new Header();
+        $this->context = '';
     }
 
 
-    public function send($value)
+    public function __clone()
     {
-        $this->value = $value;
+        $this->headers = clone $this->headers;
     }
 
-    public function run()
-    {
-        if ($this->exception) {
-            $res = $this->coroutine->throw($this->exception);
-            $this->exception = null;
-            return $res;
-        }
-        $value = $this->coroutine->current();
-        $this->coroutine->next();
 
-        return $value;
+    /*
+     * set content-type = json,and response json
+     * @param array | iterator $data
+     * */
+    public function json(array $data)
+    {
+        $response = $this->withHeader('Content-Type', 'application/json');
+        return $response->end($data);
     }
 
-    public function isFinish()
+    /*
+     * finish request
+     * Note: This method is not part of the PSR-7 standard.
+     *
+     * @param mix $data
+     * @return object
+     * */
+    public function end($data = [])
     {
-        return !$this->coroutine->valid();
-    }
-
-    public function reject($message)
-    {
-        $this->exception = $message;
-    }
-
-    public function stack(Generator $gen)
-    {
-        $coStack = new SplStack;
-        $value = null;
-        for(;;) {
-            try {
-                if ($this->exception) {
-                    $gen->throw($this->exception);
-                    $this->exception = null;
-                    continue;
-                }
-
-                if ($gen->valid()) {
-                    $value = $gen->current();
-                }
-                if ($value instanceof Generator) {
-                    $coStack->push($gen); // 保存当前的 generator
-                    $gen = $value;
-                    continue;
-                }
-
-                if (!$gen->valid()) {
-                    if ($coStack->isEmpty()) {
-                        return $value;
-                    }
-
-                    if ($this->return) {
-                        $value = $gen->getReturn();
-                    }
-                    $gen = $coStack->pop();
-                    $gen->send($value);
-                    yield $value;
-                    continue;
-                }
-
-                $gen->send($value);
-                yield $value;
-
-            } catch (RuntimeException $e) {
-                if ($coStack->isEmpty()) {
-                    throw $e;
-                }
-
-                $gen = $coStack->pop();
-                $this->exception = $e;
-            }
+        if (is_array($data)) {
+            $data = json_encode($data);
         }
 
-        return $value;
+        $this->content = $data;
+
+        return $this;
     }
+
+    /**
+     * @return Reply
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+
+    // ===================== PSR-7 standard ===================== //
+
+    public function withStatus($code, $reasonPhrase = '')
+    {
+        if (!is_string($reasonPhrase) && !method_exists($reasonPhrase, '__toString')) {
+            throw new InvalidArgumentException('ReasonPhrase must be a string');
+        }
+        $clone = clone $this;
+        $clone->statusCode = $code;
+        if ($reasonPhrase === '' && isset(Header::$messages[$code])) {
+            $reasonPhrase = Header::$messages[$code];
+        }
+        $clone->reasonPhrase = $reasonPhrase;
+
+        return $clone;
+    }
+
+    /*
+     * set response header
+     * @param string $field
+     * @param mixed $value
+     * @return void
+     * */
+    public function withHeader($field, $value)
+    {
+        $this->headers->setHeader($field, $value);
+        $clone = clone $this;
+
+        return $clone;
+    }
+
+    /*
+     * get all response headers
+     * */
+    public function getHeaders()
+    {
+        return $this->headers->getHeaders();
+    }
+
+
+    /*
+     * get header by key
+     * */
+    public function getHeader($key, $default = null)
+    {
+        return isset($this->headers[$key]) ? $this->headers[$key] : $default;
+    }
+
+
+    public function __toString()
+    {
+        return (string)$this->content;
+    }
+
+    /**
+     * Gets the response status code.
+     *
+     * The status code is a 3-digit integer result code of the server's attempt
+     * to understand and satisfy the request.
+     *
+     * @return int Status code.
+     */
+    public function getStatusCode()
+    {
+        return $this->statusCode;
+    }
+
+
+    /**
+     * Gets the response reason phrase associated with the status code.
+     *
+     * Because a reason phrase is not a required element in a response
+     * status line, the reason phrase value MAY be null. Implementations MAY
+     * choose to return the default RFC 7231 recommended reason phrase (or those
+     * listed in the IANA HTTP Status Code Registry) for the response's
+     * status code.
+     *
+     * @link http://tools.ietf.org/html/rfc7231#section-6
+     * @link http://www.iana.org/assignments/http-status-codes/http-status-codes.xhtml
+     * @return string Reason phrase; must return an empty string if none present.
+     */
+    public function getReasonPhrase()
+    {
+
+        if ($this->reasonPhrase) {
+            return $this->reasonPhrase;
+        }
+        if (isset(Header::$messages[$this->statusCode])) {
+            return Header::$messages[$this->statusCode];
+        }
+        return '';
+    }
+
 }
